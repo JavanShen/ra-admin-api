4c83c2724737ff8b6eb3f8ea35045f01c1eadcfe
apine <1985486089@qq.com>
2022-12-15 18:26:28 +0800

    :sparkles: 请求token验证

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,38 @@
 import express from 'express'
 import { UserRouter } from '../routes'
+import jwt from '../utils/jwt'
+import { whiteListUrl } from '../whiteList' 
+import { verifyUserExist } from '../utils/validate'
+
+import type { Payload } from '../types/user'
 
 const app = express()
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 app.use((req, res, next) => {
-    console.log('before request')
+    const { url, headers } = req
+
+    if (whiteListUrl.includes(url)) return next()
+    const token = headers['authorization']?.replace(/^Bearer\s/, '')
+
+    if (!token) return res.status(401).send({ errCode: -1, errInfo: '缺少token' })
+
+    const payload = jwt.verify(token) as Payload
+
+    if (!payload) return res.status(401).send({ errCode: -1, errInfo: 'token过期' })
+    
+    const { username } = payload
+
+    if(!verifyUserExist(username)) return res.status(401).send({errCode: -1, errInfo: '无效token'})
+
+    const newToken = jwt.generate({
+        username
+    }, '1d')
+
+    res.header('Authorization', newToken)
+
     next()
 })
 
